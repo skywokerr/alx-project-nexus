7cb337473d822ab430b7df2488e32890ca8045a8
Michael Atolwa <atolwamichael7@gmail.com>
2025-08-07 15:08:43 +0300

    new commits

--- movie-recommendation-backend/movie_hub/urls.py
+++ movie-recommendation-backend/movie_hub/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for proj project.
+URL configuration for movie-recommendation project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
